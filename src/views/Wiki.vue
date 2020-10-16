f88d2cf3c8b7a0a04c68d1249e79a6f32d9303b2
Arseny Malkov <arsenymalkov@gmail.com>
2020-10-16 20:58:28 +1300

    Add wiki view

---
+++ src/views/Wiki.vue
@@ -0,0 +1,18 @@
+<template>
+  <Nav />
+  <div class="wiki">
+    <h1>This is an wiki page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Nav from "@/components/Nav.vue";
+
+@Options({
+  components: {
+    Nav
+  },
+})
+export default class Wiki extends Vue {}
+</script>

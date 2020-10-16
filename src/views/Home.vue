8b78151a5f962176870ec4b88ba3f37c90df3206
Arseny Malkov <arsenymalkov@gmail.com>
2020-10-16 21:00:39 +1300

    Add main and nav component to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <Main msg="Welcome to Your Vue.js + TypeScript App" />
+    <Nav />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Options, Vue } from "vue-class-component";
+import Main from "@/components/Main.vue"; // @ is an alias to /src
+import Nav from "@/components/Nav.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Main,
+    Nav
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped>
+#nav {
+  position: fixed;
+}
+</style>

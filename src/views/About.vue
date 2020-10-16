f8f01e5bc964a33fd4b20a51e494018434343030
Arseny Malkov <arsenymalkov@gmail.com>
2020-10-16 20:54:19 +1300

    Add about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
+  <Nav />
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Nav from "@/components/Nav.vue";
+
+@Options({
+  components: {
+    Nav
+  },
+})
+export default class About extends Vue {}
+</script>

e976d0ffc5b5aeb5834f5e563438a544a61d316e
Arseny Malkov <arsenymalkov@gmail.com>
2020-10-16 20:58:19 +1300

    Add wikiitem component

---
+++ src/components/WikiItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wiki-item">
+    <h1>Wiki Item</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    // msg: String
+  }
+})
+export default class WikiItem extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

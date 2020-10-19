18097e543ee920764602033741f5dfa55adce050
Arseny Malkov <arsenymalkov@gmail.com>
2020-10-20 00:10:28 +1300

    Add prototype WikiItem to Wiki view

--- src/views/Wiki.vue
+++ src/views/Wiki.vue
@@ -1,17 +1,24 @@
 <template>
   <Nav />
   <div class="wiki">
-    <h1>This is an wiki page</h1>
+    <WikiItem />
+    <!-- <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Nav from "@/components/Nav.vue";
+import WikiItem from "@/components/WikiItem.vue";
 
 @Options({
   components: {
-    Nav
+    Nav,
+    WikiItem
   },
 })
 export default class Wiki extends Vue {}

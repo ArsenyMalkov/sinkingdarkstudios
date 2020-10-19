5acb6a43609eef97404a5869b753d100fba4ab3e
Arseny Malkov <arsenymalkov@gmail.com>
2020-10-20 00:11:00 +1300

    Create card for wiki item

--- src/components/WikiItem.vue
+++ src/components/WikiItem.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="wiki-item">
-    <h1>Wiki Item</h1>
+    <img src="../assets/logo.svg" />
+    <div class="content">
+      <router-link to="/about">Name</router-link>
+    </div>
   </div>
 </template>
 
@@ -17,5 +20,30 @@ export default class WikiItem extends Vue {}
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.wiki-item {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  margin-left: 20px;
+  margin-top: 20px;
+  width: 10%;
+}
 
+.content {
+  padding-top: 16px;
+  padding-bottom: 16px;
+}
+
+.wiki-item:hover {
+  box-shadow: 0 8px 8px 0 rgba(0,0,0,0.2);
+}
+
+img {
+  width: 100%;
+  border-radius: 5px 5px 0 0;
+}
+
+a {
+  color: #bb0303;
+}
 </style>
